83e7d3862769dfd560b1f52a7f238f1ba8500c10
AminTi <titiamin@icloud>
2020-05-03 10:49:48 +0200

    test

--- main.js
+++ main.js
@@ -1,69 +1,57 @@
 let hamburger = document.querySelector(".fa-hamburger")
 let ul = document.querySelector(".coran")
 
-
 hamburger.addEventListener("click", function () {
-
-    if (ul.style.display === "none") {
-        ul.style.display = "block";
+    if (ul.style.visibility === "hidden") {
+        ul.style.visibility = "visible"
         return false
     }
-    ul.style.display = "none";
+    ul.style.visibility = "hidden"
     return true
 })
 
 let date = document.querySelector(".date")
-let d = new Date();
 
-let FullYear = d.getFullYear();
-let FullMonth = d.getMonth()
-let getDay = d.getDay()
+var today = new Date()
+var currentDate =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()
 
-date.textContent = `${FullYear}/${FullMonth}/${getDay}`
-
-
-
+date.textContent = currentDate
 let wrapper = document.querySelector(".wrapper")
 let secondWrapper = document.querySelector(".secondWrapper")
-
-
 
 let city = document.querySelector("input")
 let btn = document.querySelector("button")
 
+let cors = "https://cors-anywhere.herokuapp.com/"
+
 async function getCountry(citys) {
-    let res = await fetch(`https://muslimsalat.com/${citys}/daily.json?key=API_KEY&jsoncallback=`)
+    let res = await fetch(
+        cors +
+            `https://muslimsalat.com/${citys}/daily.json?key=API_KEY&jsoncallback=`
+    )
     return res.json()
 }
 
-
 let temperature = document.querySelector(".fa-temperature-high")
-let citys = "Stockhom"
-getCountry(citys).then(data => {
+let citys = "Stockholm"
+getCountry(citys).then((data) => {
     updateUi(citys, data)
     temperature.textContent = `${data.today_weather.temperature}`
 })
 
-
-
-
-
 let clicked = false
 btn.addEventListener("click", function (e) {
-
     citys = city.value
-    getCountry(citys).then(data => {
+
+    getCountry(citys).then((data) => {
         updateUi(citys, data)
     })
 })
 
-
-
-
 let customers = document.querySelector(".customers")
 
 function updateUi(citys, data) {
-
     secondWrapper.innerHTML = `<table id="customers">
     <tr>
     <th>${citys.toUpperCase()}</th>
